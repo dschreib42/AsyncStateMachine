b15b6926f244e992a2dadf3e8417169264692bfe
Dirk Schreiber <dirk.schreiber@de.bosch.com>
2022-09-05 12:19:20 +0200

    Added support for re-formatting of state/transition names using CamelCase to word/sentence formatting

---
+++ tests/AsyncStateMachine.UnitTests/Graphs/Formatters/CamelCaseNameFormatterTests.cs
@@ -0,0 +1,33 @@
+﻿using AsyncStateMachine.Contracts;
+using AsyncStateMachine.Graphs.Formatters;
+using Xunit;
+
+namespace AsyncStateMachine.UnitTests.Graphs.Formatters
+{
+    public class CamelCaseNameFormatterTests
+    {
+        private readonly INameFormatter _formatter;
+
+        /// <summary>
+        /// Initializes a new instance of a <see cref="CamelCaseNameFormatterTests"/> class.
+        /// </summary>
+        public CamelCaseNameFormatterTests()
+        {
+            _formatter = new CamelCaseNameFormatter();
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("ABigBrownFoxJumpsOverTheFence", "A Big Brown Fox Jumps Over The Fence")]
+        [InlineData("ABuzz_Word", "A Buzz-Word")]
+        public void FormatName_Input_OutputCorrect(string input, string expected)
+        {
+            // Act
+            var output = _formatter.FormatName(input);
+
+            // Assert
+            Assert.Equal(expected, output);
+        }
+    }
+}
